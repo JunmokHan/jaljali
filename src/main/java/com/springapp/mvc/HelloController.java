62cd4b724eaeb9a266ad7e9d095bd9f4af2b085c
music <kalissma790@naver.com>
2016-06-08 23:02:42 +0900

    디펜던시 추가

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -11,7 +11,6 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
-		// Test
 		return "hello";
 	}
 }
